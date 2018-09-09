947e02bcc29874b7a2c39d70e57bfecea1a23b54
Osama Ilyas <osamailyas257@gmail.com>
2018-09-09 07:13:54 +0500

    Library

---
+++ js/app.js
@@ -0,0 +1,12 @@
+var user_name="osama";
+var password="books";
+
+    function button_check() {
+window.location="FirstPage/FirstPage.html";
+
+    // alert('You have suucessfully logged in :)');
+}
+
+// function library_click() {
+// window.location="features.html";
+// }
